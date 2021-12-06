4b6ccf13d8f01b35c51cf5a7d9d0461c5e6cb05a
lyqht <estee.tsw@gmail.com>
2021-12-06 12:38:42 +0800

    add button jiggle effect

--- src/components/Keyboard.tsx
+++ src/components/Keyboard.tsx
@@ -1,12 +1,22 @@
 import keyboardButtonLocations from "./keyboard-button-locations.json";
-import { useEffect } from 'preact/hooks'
+import { useEffect, useState, StateUpdater } from 'preact/hooks'
+import "preact/debug";
 
 type KeyboardKey = {
-  key: String,
-  code: String
+  key: string,
+  code: string
 }
 
-const mapKeyToIcon = (key: String) => {
+const allKeys: KeyboardKey[] = [].concat.apply([], Object.values(keyboardButtonLocations))
+const getRandomKey = () => {
+  return allKeys[Math.floor(Math.random() * allKeys.length)]
+}
+
+const getButtonElement = (code: string) : HTMLButtonElement => {
+    return document.querySelector(`[title="${code}"]`);
+}
+
+const mapKeyToIcon = (key: string) => {
   switch (key) {
     case "Backspace":
       return "<-"
@@ -18,27 +28,47 @@ const mapKeyToIcon = (key: String) => {
 }
 
 const Keyboard = () => {
+  const [jiggleKey, setJiggleKey]: [KeyboardKey, StateUpdater<KeyboardKey>] = useState(getRandomKey());
+  const makeKeyboardButtonJiggle = (buttonToJiggle: HTMLButtonElement) => {
+    buttonToJiggle.classList.add("jiggle")
+    buttonToJiggle.addEventListener("keyup", onJiggleKeyUp)
+  }
+
   useEffect(() => {
     document.addEventListener("keydown", onKeyboardButtonDown)
     document.addEventListener("keyup", onKeyboardButtonUp)
+    const buttonToJiggle = getButtonElement(jiggleKey.code)
+    makeKeyboardButtonJiggle(buttonToJiggle)
+    
     return () => {
       document.removeEventListener("keydown", onKeyboardButtonDown)
+      document.addEventListener("keyup", onKeyboardButtonUp)
     }
   }, [])
+  
+  useEffect(() => {
+    const buttonToJiggle = getButtonElement(jiggleKey.code)
+    makeKeyboardButtonJiggle(buttonToJiggle)
+  }, [jiggleKey])
 
   const onKeyboardButtonDown = (event: KeyboardEvent) => {
     event.preventDefault()
-    const title = event.code
-    const button: HTMLButtonElement = document.querySelector(`[title="${title}"]`);
+    const button: HTMLButtonElement = getButtonElement(event.code)
     button.click();
     button.focus();
   }
 
   const onKeyboardButtonUp = (event: KeyboardEvent) => {
     event.preventDefault()
-    const title = event.code
-    const button: HTMLButtonElement = document.querySelector(`[title="${title}"]`);
+    const button: HTMLButtonElement = getButtonElement(event.code)
     button.blur();
+  }
+
+  const onJiggleKeyUp = (event: KeyboardEvent) => {
+    event.preventDefault()
+    const buttonToJiggle = getButtonElement(jiggleKey.code)
+    buttonToJiggle.classList.remove("jiggle")
+    setJiggleKey(getRandomKey())
   }
 
   const KeyboardRows = () => Object.keys(keyboardButtonLocations).map(
